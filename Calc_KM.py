fdd7f420f60db7dc529d035ef29ddfffc7546cde
Keela <102982602+TicciTock188@users.noreply.github.com>
2022-04-05 11:43:09 -0400

    Add files via upload

---
+++ Calc_KM.py
@@ -0,0 +1,4 @@
+first_number = int(input("What is the first number you would like to add?"))
+second_number = int(input("What is the second number you would like to add?"))
+sum = first_number + second_number
+print("The sum of your two numbers is", sum)
